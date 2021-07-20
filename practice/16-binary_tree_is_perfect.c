c08f2717a94a7ce4f2a493d52bc84c04bda7461c
yoyogold-a11 <tmrfack@gmail.com>
2021-07-20 06:20:00 -0500

    task16

--- practice/16-binary_tree_is_perfect.c
+++ practice/16-binary_tree_is_perfect.c
@@ -32,3 +32,32 @@ size_t binary_tree_height(const binary_tree_t *tree)
     else
         return (1 + right);
 }
+
+/**
+ * binary_tree_is_perfect - get height
+ * @tree: node to check
+ * Return: nothing
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+    binary_tree_t *left, *right;
+    if (tree == NULL)
+        return (0);
+    left = tree->left;
+    right = tree->right;
+    if (binary_tree_is_leaf(tree))
+        return (1);
+    if (left == NULL || right == NULL)
+        return (0);
+
+    if (binary_tree_height(left) == binary_tree_height(right))
+    {
+        if (binary_tree_is_perfect(left) && binary_tree_is_perfect(right))
+        {
+            return (1);
+        }
+    }
+
+    return (0);
+}
