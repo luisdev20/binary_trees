3cfb40b70a2bb6d7768ee2e7b8fd606ad277d98e
yoyogold-a11 <tmrfack@gmail.com>
2021-07-22 02:59:47 -0500

    task25

--- binary_trees.h
+++ binary_trees.h
@@ -68,6 +68,7 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 int greater(const binary_tree_t *tree, int n);
 int less(const binary_tree_t *tree, int n);
+bst_t *bst_insert(bst_t **tree, int value);
 
 
 #endif /* _BINARY_TREES_H_ */
