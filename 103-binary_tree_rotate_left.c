3b8259937c478eb043c4ef0419911f98ec6c8d85
yoyogold-a11 <tmrfack@gmail.com>
2021-07-21 23:38:57 -0500

    task22

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,19 +8,19 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (NULL);
-    tree->parent = tree->right;
-    if (tree->right != NULL)
-    {
-        tree->right = tree->right->left;
-        tree->parent->left = tree;
-        tree->parent->parent = NULL;
-        if (tree->right != NULL)
-        {
-            tree->right->parent = tree;
-        }
-        return (tree->parent);
-    }
-    return (tree);
+	if (tree == NULL)
+		return (NULL);
+	tree->parent = tree->right;
+	if (tree->right != NULL)
+	{
+		tree->right = tree->right->left;
+		tree->parent->left = tree;
+		tree->parent->parent = NULL;
+		if (tree->right != NULL)
+		{
+			tree->right->parent = tree;
+		}
+		return (tree->parent);
+	}
+	return (tree);
 }
