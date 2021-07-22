a87083779a1d183dd2ea704b945d951ca4f8879f
yoyogold-a11 <tmrfack@gmail.com>
2021-07-22 12:03:12 -0500

    fixing

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,26 +8,27 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *new_root;
+	binary_tree_t *pivot, *tmp;
 
 	if (tree == NULL || tree->right == NULL)
-		return (tree);
+		return (NULL);
 
-	new_root = tree->right;
-
-	if (new_root->left)
+	pivot = tree->right;
+	tmp = pivot->left;
+	pivot->left = tree;
+	tree->right = tmp;
+	if (tmp != NULL)
+		tmp->parent = tree;
+	tmp = tree->parent;
+	tree->parent = pivot;
+	pivot->parent = tmp;
+	if (tmp != NULL)
 	{
-		tree->right = new_root->left;
-		new_root->left->parent = tree;
-	}
-	else
-	{
-		tree->right = NULL;
+		if (tmp->left == tree)
+			tmp->left = pivot;
+		else
+			tmp->right = pivot;
 	}
 
-	new_root->left = tree;
-	new_root->parent = tree->parent;
-	tree->parent = new_root;
-
-	return (new_root);
+	return (pivot);
 }
