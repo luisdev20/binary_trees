b02ab09f6675e2ebdfc1ec10eed1e85d987f8b97
yoyogold-a11 <tmrfack@gmail.com>
2021-07-22 05:14:19 -0500

    task30

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,19 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * array_to_bst - insert a new node in a Binary Search Tree
- * @array: arrya to use
- * @size: array size
+ * binary_tree_is_avl - insert a new node in a Binary Search Tree
+ * @tree: arrya to use
  *
  * Return: If no common ancestor was found return NULL
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-    unsigned int z;
-    if (binary_tree_is_bst(tree) != 0 && binary_tree_is_perfect(tree) != 0)
-        return (1);
-    else
-        return (0);
+	if (!tree)
+	{
+		return (0);
+	}
+	if (!tree->left && !tree->right)
+		return (1);
+
+	if (binary_tree_is_bst(tree) != 1 || binary_tree_balance(tree) != 1)
+		return (0);
+	if (binary_tree_is_avl(tree->right) != 1)
+		return (0);
+
+	return (1);
 }
 
 /**
@@ -24,20 +31,20 @@ int binary_tree_is_avl(const binary_tree_t *tree)
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if (less(tree->left, tree->n) && greater(tree->right, tree->n))
-    {
-        if (!tree->left || binary_tree_is_bst(tree->left))
-        {
-            if (!tree->right || binary_tree_is_bst(tree->right))
-            {
-                return (1);
-            }
-        }
-    }
-    return (0);
+	if (less(tree->left, tree->n) && greater(tree->right, tree->n))
+	{
+		if (!tree->left || binary_tree_is_bst(tree->left))
+		{
+			if (!tree->right || binary_tree_is_bst(tree->right))
+			{
+				return (1);
+			}
+		}
+	}
+	return (0);
 }
 
 /**
@@ -48,20 +55,20 @@ int binary_tree_is_bst(const binary_tree_t *tree)
  */
 int less(const binary_tree_t *tree, int n)
 {
-    int left, right;
+	int left, right;
 
-    if (tree == NULL)
-        return (1);
-    if (tree->n < n)
-    {
-        left = less(tree->left, n);
-        right = less(tree->right, n);
-        if (left != 0 && right != 0)
-        {
-            return (1);
-        }
-    }
-    return (0);
+	if (tree == NULL)
+		return (1);
+	if (tree->n < n)
+	{
+		left = less(tree->left, n);
+		right = less(tree->right, n);
+		if (left != 0 && right != 0)
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
 
 /**
@@ -72,114 +79,69 @@ int less(const binary_tree_t *tree, int n)
  */
 int greater(const binary_tree_t *tree, int n)
 {
-    int left, right;
+	int left, right;
 
-    if (tree == NULL)
-        return (1);
+	if (tree == NULL)
+		return (1);
 
-    if (tree->n > n)
-    {
-        left = greater(tree->left, n);
-        right = greater(tree->right, n);
-        if (left != 0 && right != 0)
-        {
-            return (1);
-        }
-    }
-    return (0);
+	if (tree->n > n)
+	{
+		left = greater(tree->left, n);
+		right = greater(tree->right, n);
+		if (left != 0 && right != 0)
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
 
 /**
- * binary_tree_depth - Measures the depth of a node in a binary tree.
- * @tree: A pointer to the node to measure the depth.
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
  *
- * Return: If tree is NULL, your function must return 0, else return the depth.
+ * Return: If tree is NULL, your function must return 0, else return height.
  */
-size_t binary_tree_depth(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t count = 0;
-
-	if (!tree || !tree->parent)
-		return (count);
-
-	count = 1 + binary_tree_depth(tree->parent);
-
-	return (count);
-}
-
-/**
- * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree.
- * @tree: A pointer to the root node of the tree to count the number of nodes.
- *
- * Return: If tree is NULL, the function must return 0, else return node count.
- */
-size_t binary_tree_nodes(const binary_tree_t *tree)
-{
-	size_t count = 0;
+	size_t left = 0, right = 0;
 
 	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
-	count += 1;
-	count += binary_tree_nodes(tree->left);
-	count += binary_tree_nodes(tree->right);
+	left = 1 + binary_tree_height(tree->left);
+	right = 1 + binary_tree_height(tree->right);
 
-	return (count);
-
+	if (left > right)
+		return (left);
+	return (right);
 }
 
 /**
- * binary_tree_is_full - Checks if a binary tree is full.
- * @tree: A pointer to the root node of the tree to check.
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the balance factor.
  *
- * Return: If tree is NULL or is not full - 0.
- *         Otherwise - 1.
+ * Return: If tree is NULL, return 0, else return balance factor.
  */
-int binary_tree_is_full(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
-	int l, r;
+	int left_height = 0, right_height = 0;
 
 	if (!tree)
 		return (0);
 
-	if (!tree->left && !tree->right)
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	if (left_height - right_height == 0)
+		return (1);
+	if (left_height - right_height == 1)
+		return (1);
+	if (left_height - right_height == -1)
 		return (1);
 
-	if (tree->left && tree->right)
-	{
-		l = binary_tree_is_full(tree->left);
-		r = binary_tree_is_full(tree->right);
-		return (l * r);
-	}
 	return (0);
-
-}
-
-/**
- * binary_tree_is_perfect - Checks if a binary tree is perfect.
- * @tree: A pointer to the root node of the tree to check.
- *
- * Return: If tree is NULL or not perfect, 0.
- *         Otherwise, 1.
- */
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-	int l, r;
-
-	if (!tree)
-		return (0);
-
-	if (binary_tree_is_full(tree) == 0)
-		return (0);
-
-	if (binary_tree_depth(tree->left) == binary_tree_depth(tree->right))
-	{
-		l = binary_tree_nodes(tree->left);
-		r = binary_tree_nodes(tree->right);
-		if ((l + r) < 3)
-			return (1);
-	}
-
-	return (0);
-
 }
--- 14-main.c
+++ 14-main.c
@@ -14,13 +14,12 @@ int main(void)
 
 	root = binary_tree_node(NULL, 98);
 	root->left = binary_tree_node(root, 12);
-	root->right = binary_tree_node(root, 402);
-	binary_tree_insert_right(root->left, 54);
-	binary_tree_insert_right(root, 128);
-	binary_tree_insert_left(root, 45);
-	binary_tree_insert_right(root->left, 50);
-	binary_tree_insert_left(root->left->left, 10);
-	binary_tree_insert_left(root->left->left->left, 8);
+	root->right = binary_tree_node(root, 128);
+	root->left->right = binary_tree_node(root->left, 54);
+	root->right->right = binary_tree_node(root, 402);
+	root->left->left = binary_tree_node(root->left, 10);
+	root->right->left = binary_tree_node(root->right, 97);
+	root->right->right->right = binary_tree_node(root->right->right, 430);
 	binary_tree_print(root);
 
 	balance = binary_tree_balance(root);
